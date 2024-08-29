998c47bd59b0c281641308504293c09144490a89
Cibele Caroline de Oliveira Guidek <cibele.guidek@escola.pr.gov.br>
2024-08-29 01:30:26 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+ const primeironome = "Cibele";
+ const idade = 16;
+ const altura = 1.61;
+ const cidadenatal = "Curitiba";
+ const massa = "sentoecinquentakilos";
+
+console.log("O meu primeiro nome é: Cibele, O meu sobrenome é: Caroline, O meu nome do meio é: Oliveira, e o meu ultimo nome é: Guidek. Tenho 16 anos e nasci em Curitiba, sendo um curitibannense :D.")
+console.log(primeironome);
+console.log(idade);
+console.log(altura);
+console.log(cidadenatal);
+console.log(massa);
